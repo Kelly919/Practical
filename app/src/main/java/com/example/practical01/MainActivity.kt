fea50f7b3b48f1736de4e4871acd73413a17bbe5
unknown <mickleykhoza@gmail.com>
2022-04-13 20:55:18 +0200

    adding files

---
+++ app/src/main/java/com/example/practical01/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.practical01
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.practical01.ui.theme.Practical01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+
+                Text(
+                    text = "Welcome to My Jetpack Compose Journey!!!",
+                    color = Color.Blue,
+                    fontSize = 15.sp,
+                )
+            }
+
+            Column {
+                val openDialog = remember { mutableStateOf(false) }
+                val context = LocalContext.current
+
+                Button(
+                    onClick =  {
+                        openDialog.value = true
+                    }
+                )
+
+                {
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = "Localized description",
+                        Modifier.padding(end = 8.dp),
+
+                        )
+                    Text(text = "Info")
+                }
+
+                if (openDialog.value){
+
+                    AlertDialog(onDismissRequest = {
+                        openDialog.value = false
+                    },
+                        text = {
+                            Text(  "My journey in Mobile Programming has been good." +
+                                    " I have learned many things, " +
+                                    "I cannot wait to create my own apps in future :) ....",
+                                fontSize = 10.sp,
+                                color = Color.Black)
+                        },
+                        confirmButton = {
+                            Button(
+                                onClick =  {
+                                    openDialog.value = false
+                                    Toast.makeText(context, "Confirm...", Toast.LENGTH_SHORT).show()
+                                })
+                            {
+                                Text("Confirm")
+                            }
+                        },
+
+                        dismissButton = {
+                            Button(
+                                onClick = {
+                                    openDialog.value = false
+                                    Toast.makeText(context, "Dismiss.....", Toast.LENGTH_SHORT).show()
+                                })
+                            {
+                                Text("Dismiss")
+                            }
+                        },
+                        backgroundColor = Color.Magenta
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
+
